17ed5a328d5dbfeb9bacd0c9ebbc0b9e6da0d6b4
sets018 <91557108+sets018@users.noreply.github.com>
2023-01-02 18:21:56 -0500

    Create dicts.py

---
+++ data scrapping/accounts info/dicts.py
@@ -0,0 +1,9 @@
+brand_dict_reps = {
+             "Apple" : ["Apple","AppleMusic","AppleSupport","AppleTVPlus","AppleTV","iTunes"]
+             "Samsung" : ["SamsungMobile","Samsung"],
+             "Amazon" : ["amazon","alexa99"]}
+brand_dict_ft = {
+             "Apple" : ["1","482,215","+18%","RANK UNCHANGED"],
+             "Microsoft" : ["2","278,288","+32%","UP 1 PLACE"],
+             "Samsung" : ["5","87,689","+17%","RANK UNCHANGED"],
+             "Amazon" : ["3","274,819","+10%","DOWN 1 PLACE"]}
