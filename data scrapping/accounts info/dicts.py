48488b874a01dca0d90fe6201d262a61e2c65362
sets018 <91557108+sets018@users.noreply.github.com>
2023-01-04 11:32:29 -0500

    Update dicts.py

--- data scrapping/accounts info/dicts.py
+++ data scrapping/accounts info/dicts.py
@@ -4,11 +4,36 @@ brand_dict_reps = {
              "Amazon" : ["amazon","amazonmusic","PrimeVideo","awscloud","amazonnews","alexa99","amazonluna","amazonbooks","AmazonStudios","AmazonPub","amazonfiretv","Sell_on_Amazon","amazonca","primestudent","AmazonBusiness","AmazonScience"],
              "Google" : ["Google","GooglePlay","GoogleAI","googlenews","GoogleTrends","googledrive","GoogleForEdu","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","GoogleSmallBiz","GoogleDesign","googlebooks","Googleorg","GoogleStartups","Google_Comms","GooglePay","GoogleAds","googleanalytics","ThinkwithGoogle","googlestudents","googlecalendar","googlephotos","GoogleDoodles","googledocs","googlecanada","googlefiber","googleearth","madebygoogle","googledevs","GoogleWorkspace","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","YouTube","googlechrome"],
 	     "Samsung" : ["samsungresearch","SamsungNetworks","SamsungDSGlobal","SamsungCanada","SamsungTVPlus","SamsungGaming","SamsungBizUSA","SamsungMobileUS","samsung_dev","SamsungExynos","SamsungTV","SamsungUS","Samsung","SamsungMobile"],
-             "Toyota" : ["ToyotaSoCal","Toyota","TGR_WEC","ToyotaMotorCorp","ToyotaRacing","DriveToyota","ToyotaResearch","ToyotaCanada","ToyotaFinancial"]}
+             "Toyota" : ["ToyotaSoCal","Toyota","TGR_WEC","ToyotaMotorCorp","ToyotaRacing","DriveToyota","ToyotaResearch","ToyotaCanada","ToyotaFinancial"],
+	     "McDonald’s" : ["McDonalds","McDonaldsUK","McDonaldsCanada","McDonaldsCorp"],
+             "Tesla" : ["Tesla","Tesla_Asia"],
+             "BMW" : ["BMW","BMWGroup","BMWMotorrad","BMWUSA","BMWMotorsport","BMW_UK","BMWi","BMWMotorradUSA","bmwindia","BMWMotorradUK","bmwcanada","BMWUKMotorsport","BMWMotorradMoSp"],
+             "Louis Vuitton" : ["LouisVuitton","LVMH","MoetHennessy"],
+             "Cisco" : ["Cisco","CiscoLive","CiscoNetworking","Webex","CiscoNetAcad","HeyCisco","CiscoPress","ciscoDC","CiscoCollab","ciscospaces","CiscoEvents","CiscoNetworking","CiscoIT","CiscoCX","CiscoSecure","CiscoPartners","CiscoDevNet","CiscoSoftware","LearningatCisco","Cisco_Support"],
+             "Instagram" : ["instagram","InstagramComms"]
+             "Facebook" : ["facebook","FacebookGaming","FacebookWatch","facebookapp"],
+             "IBM" : ["IBM","IBMData","IBMResearch","IBMIoT","IBMDeveloper","IBMcloud","IBMNews","IBMWatson","IBMSecurity","IBMEurope","IBMPartners","IBMservers","ibm_in","IBMBlockchain","IBMScience","IBMImpact","IBMUKI","IBMSports","IBMMEA","IBMCommunity","IBM_iX","ibmconsulting","IBMIBV","IBMpolicy","ibmdesign","IBMZ","lifeatibm","IBMCanada","IBMStorage"],
+             "Intel" : ["intel","IntelGaming","intelnews","WeAreIntel","Inteliot","IntelBusiness","IntelTech","IntelIndia","IntelIndia","intelcanada","IntelFPGA","IntelSoftware","intelcapital","IntelUK","IntelSecurity","IntelHealth","intelhpc"],
+             "SAP" : ["SAP","SAPDigitalSvcs","SAP_Jam","SAPCloudERP","SAPCommunity","SAPEDU","SAPAnalytics","SAPAriba","SAPBTB","SAPEDU","SAPAriba","SAPNews","SAPConcur","SAPMentors","SAPBusinessOne","SAPUKIreland","SCMatSAP","SAPSapphire","SAPIndustries","SAPMobile","SAPPartnerEdge","SAP_BayArea","SAPIndia","SAP_CX","SAPdevs","saptv","openSAP","saplabsindia","sapcp","SAPNorthAmerica","successfactors","SAP_Retail","LifeatSAP","SAPCXDataCloud","SAPFinance","SAPTechEd","SAPSports","SAPhightech","SAPAI","SAPNextGen","SAPBanking","sap_bpa"],
+             "Adobe" : ["Adobe","adobeanimate","Photoshop","dreamweaver","Lightroom","AdobeDesign","creativecloud","AdobeXD","Substance3D","AdobeForEdu","AdobeAero","AdobeExpCloud","InDesign","adobedevs","AdobeFonts","AdobeTCS","AdobeVideo","AdobeAE","AdobeConnect","AdobeCare","AdobeCreate","AdobeResearch","AdobeDrawing","AdobePortfolio","AdobeCommerce","AdobeMarketo","AdobeCare","AdobeCanada","AdobeUK","AdobeGenCreate","AdobeExpress","AdobeDocCloud","AdobeCareers","AdobeExpCare","AdobeNews"],
+             "Chanel" : ["CHANEL"]}
+
 brand_dict_ft = {
              "Apple" : ["1","482,215","+18%","RANK UNCHANGED"],
              "Microsoft" : ["2","278,288","+32%","UP 1 PLACE"],
              "Amazon" : ["3","274,819","+10%","DOWN 1 PLACE"],
 	     "Google" : ["4","251,751","+28%","RANK UNCHANGED"],
              "Samsung" : ["5","87,689","+17%","RANK UNCHANGED"],
-	     "Toyota" : ["6","59,757","+10%","UP 1 PLACE"]}
+	     "Toyota" : ["6","59,757","+10%","UP 1 PLACE"],
+             "McDonald’s" : ["11","48,647","+6%","DOWN 2 PLACES"],
+             "Tesla" : ["12","48,002","+32%","UP 2 PLACES"],
+             "BMW" : ["13","46,331","+11%","DOWN 1 PLACE"],
+             "Louis Vuitton" : ["14","44,508","+21%","DOWN 1 PLACE"],
+             "Cisco" : ["15","41,298","+14%","UP 1 PLACE"],
+             "Instagram" : ["16","36,516","-5%","UP 3 PLACES"]
+             "Facebook" : ["17","34,538","-5%","DOWN 2 PLACES"],
+             "IBM" : ["18","34,242","+3%","RANK UNCHANGED"],
+             "Intel" : ["19","32,916","-8%","DOWN 2 PLACES"],
+             "SAP" : ["20","31,497","+5%","RANK UNCHANGED"],
+             "Adobe" : ["21","30,660","+23%","RANK UNCHANGED"],
+             "Chanel" : ["22","29,259","+32%","RANK UNCHANGED"]}
