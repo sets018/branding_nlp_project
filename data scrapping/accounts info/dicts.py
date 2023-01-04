f6ef0d864a37c2d2d425e78260a0b41a7127f188
Ends9 <121689462+Ends9@users.noreply.github.com>
2023-01-04 01:05:50 -0500

    Update dicts.py

--- data scrapping/accounts info/dicts.py
+++ data scrapping/accounts info/dicts.py
@@ -1,9 +1,14 @@
 brand_dict_reps = {
              "Apple" : ["Apple","AppleMusic","AppleSupport","AppleTVPlus","AppleTV","iTunes"],
-             "Samsung" : ["SamsungMobile","Samsung"],
-             "Amazon" : ["amazon","alexa99"]}
+						 "Microsoft" : ["Microsoft","Azure","Outlook","MSFTnews","Windows","Office","surface","MicrosoftEdge","MSFTResearch","Microsoft365","msftsecurity","Xbox","MicrosoftTeams","MSPowerAutomate","SQLServer","MS_Ignite","msuspartner","MSPowerApps","onedrive","msdev","MicrosoftEDU","microsoftcanada","MS_ITProCA","MicrosoftEduCA","msdevCA","msft_businessCA","WindowsCanada"],
+             "Amazon" : ["amazon","amazonmusic","PrimeVideo","awscloud","amazonnews","alexa99","amazonluna","amazonbooks","AmazonStudios","AmazonPub","amazonfiretv","Sell_on_Amazon","amazonca","primestudent","AmazonBusiness","AmazonScience"],
+             "Google" : ["Google","GooglePlay","GoogleAI","googlenews","GoogleTrends","googledrive","GoogleForEdu","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","GoogleSmallBiz","GoogleDesign","googlebooks","Googleorg","GoogleStartups","Google_Comms","GooglePay","GoogleAds","googleanalytics","ThinkwithGoogle","googlestudents","googlecalendar","googlephotos","GoogleDoodles","googledocs","googlecanada","googlefiber","googleearth","madebygoogle","googledevs","GoogleWorkspace","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","YouTube","googlechrome"],
+						 "Samsung" : ["SamsungMobile","Samsung"],
+             "Toyota" : ["ToyotaSoCal","Toyota","TGR_WEC","ToyotaMotorCorp","ToyotaRacing","DriveToyota","ToyotaResearch","ToyotaCanada","ToyotaFinancial"],
 brand_dict_ft = {
              "Apple" : ["1","482,215","+18%","RANK UNCHANGED"],
              "Microsoft" : ["2","278,288","+32%","UP 1 PLACE"],
-             "Samsung" : ["5","87,689","+17%","RANK UNCHANGED"],
              "Amazon" : ["3","274,819","+10%","DOWN 1 PLACE"]}
+						 "Google" : ["4","251,751","+28%","RANK UNCHANGED"]}
+             "Samsung" : ["5","87,689","+17%","RANK UNCHANGED"],
+					   "Toyota" : ["6","59,757","+10%","UP 1 PLACE"],
