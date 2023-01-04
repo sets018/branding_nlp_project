14dbbf65747d087828409f39ae842301bf808766
sets018 <91557108+sets018@users.noreply.github.com>
2023-01-04 10:10:22 -0500

    Update dicts.py

--- data scrapping/accounts info/dicts.py
+++ data scrapping/accounts info/dicts.py
@@ -4,11 +4,11 @@ brand_dict_reps = {
              "Amazon" : ["amazon","amazonmusic","PrimeVideo","awscloud","amazonnews","alexa99","amazonluna","amazonbooks","AmazonStudios","AmazonPub","amazonfiretv","Sell_on_Amazon","amazonca","primestudent","AmazonBusiness","AmazonScience"],
              "Google" : ["Google","GooglePlay","GoogleAI","googlenews","GoogleTrends","googledrive","GoogleForEdu","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","GoogleSmallBiz","GoogleDesign","googlebooks","Googleorg","GoogleStartups","Google_Comms","GooglePay","GoogleAds","googleanalytics","ThinkwithGoogle","googlestudents","googlecalendar","googlephotos","GoogleDoodles","googledocs","googlecanada","googlefiber","googleearth","madebygoogle","googledevs","GoogleWorkspace","GoogleOSS","lifeatgoogle","googlefi","GoogleInCA","YouTube","googlechrome"],
 	     "Samsung" : ["samsungresearch","SamsungNetworks","SamsungDSGlobal","SamsungCanada","SamsungTVPlus","SamsungGaming","SamsungBizUSA","SamsungMobileUS","samsung_dev","SamsungExynos","SamsungTV","SamsungUS","Samsung","SamsungMobile"],
-             "Toyota" : ["ToyotaSoCal","Toyota","TGR_WEC","ToyotaMotorCorp","ToyotaRacing","DriveToyota","ToyotaResearch","ToyotaCanada","ToyotaFinancial"],
+             "Toyota" : ["ToyotaSoCal","Toyota","TGR_WEC","ToyotaMotorCorp","ToyotaRacing","DriveToyota","ToyotaResearch","ToyotaCanada","ToyotaFinancial"]}
 brand_dict_ft = {
              "Apple" : ["1","482,215","+18%","RANK UNCHANGED"],
              "Microsoft" : ["2","278,288","+32%","UP 1 PLACE"],
              "Amazon" : ["3","274,819","+10%","DOWN 1 PLACE"],
 	     "Google" : ["4","251,751","+28%","RANK UNCHANGED"],
              "Samsung" : ["5","87,689","+17%","RANK UNCHANGED"],
-	     "Toyota" : ["6","59,757","+10%","UP 1 PLACE"],
+	     "Toyota" : ["6","59,757","+10%","UP 1 PLACE"]}
