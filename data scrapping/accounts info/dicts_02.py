17d63c01ff595d768e8652866315cb4aa54806b3
sets018 <91557108+sets018@users.noreply.github.com>
2023-01-18 22:07:25 -0500

    Create dicts_02.py

---
+++ data scrapping/accounts info/dicts_02.py
@@ -0,0 +1,33 @@
+brand_dict_reps = {
+             "Hermès" : ["Hermes_Paris"],
+             "J.P. Morgan" : ["jpmorgan","JPMWealth","JPMorganAM"],
+             "YouTube" : ["TeamYouTube","YouTube","YouTubeCreators","youtubemusic","YouTubeGaming","YouTubeIndia"],
+             "Honda" : ["Honda","HondaCanada","Honda_UK","HondaCustSvc"],
+             "American Express" : ["AmericanExpress","AskAmex","AmexBusiness"],
+             "IKEA" : ["IKEA","IKEAUK","IKEAUSA","IKEACanada"],
+             "UPS" : ["TheUPSStore","UPS","UPSers"],
+             "Gucci" : ["gucci"],
+             "Accenture" : ["Accenture","AccentureStrat"],
+             "Pepsi" : ["PepsiCanada","PepsiMaxUK","pepsi","PepsiGlobal"],
+             "GE" : ["generalelectric","GE_Canada","GE_Appliances","GEpublicaffairs"],
+             "Allianz" : ["Allianz","allianzuknews","AllianzPartners"],
+             "Hyundai" : ["Hyundai_Global","Hyundai","HyundaiCanada","Hyundai_UK"],
+             "Salesforce" : ["salesforce","SalesforceOrg","SalesforceNews","SalesforceAdmns","SalesforceDevs],
+}
+
+brand_dict_ft = {
+             "Hermès" : ["23","27,398","+27%","RANK UNCHANGED"],
+             "J.P. Morgan" : ["24","24,335","+14%","RANK UNCHANGED"],
+             "YouTube" : ["25","24,268","+16%","UP 1 PLACE"],
+             "Honda" : ["26","22,837","+7%","DOWN 1 PLACE"],
+             "American Express" : ["27","22,117","+16%","UP 3 PLACES"],
+             "IKEA" : ["28","21,928","+9%","DOWN 1 PLACE"],
+             "UPS" : ["29","21,164","+9%","RANK UNCHANGED"],
+             "Gucci" : ["30","20,417","+23%","UP 3 PLACES"],
+             "Accenture" : ["31","20,412","+15%","UP 1 PLACE"],
+             "Pepsi" : ["32","19,622","+1%","DOWN 4 PLACES"],
+             "GE" : ["33","19,093","+4%","DOWN 2 PLACES"],
+             "Allianz" : ["34","18,694","+23%","RANK UNCHANGED"],
+             "Hyundai" : ["35","17,314","+14%","RANK UNCHANGED"],
+             "Salesforce" : ["36","17,311","+17%","UP 2 PLACES"],
+             }
