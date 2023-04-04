60f6e299126d861c09db6f6bc11775afd1921d6c
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-04 00:49:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 import plotly.figure_factory as ff
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query("SELECT * FROM `tweets_data`")
+
+data = pd.read_sql(rows)
 
 st.set_page_config(
     page_title="Twitter_EDA",
