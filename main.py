9a6fb76f5f2d8f5dc4f933ceb40ad40680dea295
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-03 23:31:14 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Importing libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+import plotly.figure_factory as ff
+
+st.set_page_config(
+    page_title="Twitter_EDA",
+    layout="centered",
+    initial_sidebar_state="auto",
+)
+
+st.title('Twitter_EDA')
+st.write('idk what im doing with my life')
+
+cat_input = []
+num_input = []
