3d6f211bccb50155feae128d7dc33fbe07a8cc39
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-04 22:56:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,38 +9,28 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import plotly.express as px
 
-class user_input():
-    def __init__(self, var, type, data, type_data, input_list):
-        self.var = var
-        self.type = type
+class plotting():
+    def __init__(self, data, group_by, y_var, stat):
         self.data = data
-        self.type_data = type_data
-        self.input_list = input_list
-        self.get_input()
-    def get_input(self):
-        self.user_input = 'placeholder'
-        if (self.type == 'radio'):
-            self.get_radio()
-        elif (self.type == 'slider'):
-            self.get_slider()
-        self.input_list.append(self.user_input)
-    def get_radio(self):
-        if (self.type_data == 'dataframe'):
-            self.user_input = st.radio(
-                self.var,
-                np.unique(self.data.data_source[self.var]))
-        elif (self.type_data == 'list'):
-            self.user_input = st.radio(
-                self.var,
-                self.data)
-        st.write(self.var,": ",self.user_input)
-    def get_slider(self):
-        if (self.type_data == 'dataframe'):
-            self.user_input = st.slider(self.var, 0, max(self.data.data_source[self.var]), 1)
-        elif (self.type_data == 'list'):
-            self.user_input = st.slider(self.var, 0, max(self.data), 1)
-        st.write(self.var,": ",self.user_input)
-        
+        self.group_by = group_by
+        self.stat = stat
+        self.y_var = y_var
+    def show_plot(self):
+        get_fig()
+        st.plotly_chart(self.fig)
+    def prepare_data(self):
+        if (stat = "Count"):
+            self.data_plot = data.groupby(histx_option).count()[self.y_var]
+        if (stat = "Sum"):
+            self.data_plot = data.groupby(histx_option).sum()[self.y_var]
+        if (stat = "Mean"):
+            self.data_plot = data.groupby(histx_option).mean()[self.y_var]
+        self.data_plot = self.data_plot.to_frame()
+        self.data_plot = self.data_plot.reset_index()
+    def get_fig(self):
+        self.fig = px.histogram(self.data_plot, x = group_by, y = y_var
+                   marginal="box",
+                   hover_data=self.data_plot)
 st.set_page_config(
     page_title="Twitter_EDA",
     layout="centered",
@@ -68,7 +58,7 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT * FROM `branding-nlp-project.twitter_data.tweets_merged_all`")
+rows = run_query("SELECT * FROM `branding-nlp-project.twitter_data.twitter_data_merged`")
 df = pd.DataFrame(rows)
 st.write("Data head")
 st.write(df.head())
@@ -81,3 +71,27 @@ df_stats  = df.describe().apply(lambda s: s.apply(lambda x: format(x, 'f')))
 df_num_stats = df_stats[["Retweet_Count", "Quote_Count", "Like_Count", "Word_count", "Emoji_count"]]
 st.dataframe(df_num_stats)
 st.write("Graphsssssssss")
+histg = st.checkbox("Distribution histogram with x axis as a cualitative var")
+if histg:
+    st.write("Distribution histogram with x axis as a cualitative var")
+    histx_option = st.selectbox(
+            "Select x time var",
+            ("Day", "Month", "Year", "Day_Month", "Month_Year"),
+            label_visibility=st.session_state.visibility,
+            disabled=st.session_state.disabled,
+    )
+    stat =  st.radio(
+        "Select stat for y axis var",
+        options=["Count","Mean","Sum"],
+    )
+    if (stat == "Count"): 
+        y_poss_options = ("#_Tweets")
+    else: 
+        y_poss_options = ("Retweet_Count", "Quote_Count", "Like_Count", "Emoji_count", "Word_count")
+        
+    histy_option = st.selectbox(
+            "Select y var",
+            y_poss_options,
+        )
+    var_time_plot = plotting(df, histx_option, histy_option, stat)
+    var_time_plot.show_plot()
