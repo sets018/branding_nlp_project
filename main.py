0261df43c41fa9de5b7374ac0cda2f9884cbf117
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-05 15:02:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,46 @@ from google.cloud import bigquery
 import plotly.express as px
 
 class plotting():
-    def __init__(self, data, group_by, y_var, stat):
+    def __init__(self, data, group_by, y_var, stat, color_var):
         self.data = data
         self.group_by = group_by
         self.stat = stat
         self.y_var = y_var
         self.prepare_data()
+        self.color_var = color_var 
     def show_plot(self):
         self.get_fig()
         st.plotly_chart(self.fig)
     def prepare_data(self):
-        if (self.stat == "Count"):
-            self.data_plot = self.data.groupby(self.group_by).count()[self.y_var]
-        if (self.stat == "Sum"):
-            self.data_plot = self.data.groupby(self.group_by).sum()[self.y_var]
-        if (self.stat == "Mean"):
-            self.data_plot = self.data.groupby(self.group_by).mean()[self.y_var]
+        if (self.color_var == None): 
+            if (self.stat == "Count"):
+                self.data_plot = self.data.groupby(self.group_by).count()[self.y_var]
+            if (self.stat == "Sum"):
+                self.data_plot = self.data.groupby(self.group_by).sum()[self.y_var]
+            if (self.stat == "Mean"):
+                self.data_plot = self.data.groupby(self.group_by).mean()[self.y_var]
+        else: 
+            if (self.stat == "Count"):
+                self.data_plot = self.data.groupby(self.group_by).count()[[self.y_var,self.color_var]]
+            if (self.stat == "Sum"):
+                self.data_plot = self.data.groupby(self.group_by).sum()[[self.y_var,self.color_var]]
+            if (self.stat == "Mean"):
+                self.data_plot = self.data.groupby(self.group_by).mean()[[self.y_var,self.color_var]]
         self.data_plot = self.data_plot.to_frame()
         self.data_plot = self.data_plot.reset_index()
         st.write(self.data_plot)
     def get_fig(self):
-        if (self.stat == "Count"):
-            self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"# Tweets distribution over {self.group_by}s")
-        else:
-            self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"{self.y_var} {self.stat} distribution over {self.group_by}s")
+        if (self.color_var == None): 
+            if (self.stat == "Count"):
+                self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"# Tweets distribution over {self.group_by}s")
+            else:
+                self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"{self.y_var} {self.stat} distribution over {self.group_by}s")
+        else: 
+            if (self.stat == "Count"):
+                self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"# Tweets distribution over {self.group_by}s color {self.color_var}", color=self.color_var)
+            else:
+                self.fig = px.bar(self.data_plot, x = self.group_by, y = self.y_var, title=f"{self.y_var} {self.stat} distribution over {self.group_by}s  color {self.color_var}", color=self.color_var)
+            
         
 st.set_page_config(
     page_title="Twitter_EDA",
@@ -87,7 +103,7 @@ histg = st.checkbox("Distribution histogram with x axis as a cualitative var")
 if histg:
     histx_option = st.selectbox(
             "Select x time var",
-            ("Day", "Month", "Year", "Day_Month", "Month_Year")
+            ("Day", "Month", "Day_Month")
     )
     stat =  st.radio(
         "Select stat for y axis var",
@@ -103,7 +119,49 @@ if histg:
                 y_poss_options
             )
     
-    var_time_plot = plotting(df, histx_option, histy_option, stat)
+    var_time_plot = plotting(df, histx_option, histy_option, stat, None, None)
+
+    def get_plot_time(var_time_plot): 
+        var_time_plot.show_plot()
+     
+    get_plot_time(var_time_plot)
+
+histg_color = st.checkbox("Distribution histogram with x axis as a cualitative var with co=lo=ur")
+
+if histg_color:
+    histx_option_2 = st.selectbox(
+            "Select x time var",
+            ("Day", "Month", "Day_Month")
+    )
+    stat_2 =  st.radio(
+        "Select stat for y axis var",
+        options=["Count","Mean","Sum"]
+    )
+    if (stat_2 == "Count"): 
+        y_poss_options_2 = ("#_Tweets")
+        histy_option_2 = "Retweet_Count"
+    else: 
+        y_poss_options_2 = ("Retweet_Count", "Quote_Count", "Like_Count", "Emoji_count", "Word_count")  
+        histy_option_2 = st.selectbox(
+                "Select y var",
+                y_poss_options_2
+            )  
+    color_stat =  st.radio(
+        "Select stat for y axis color var",
+        options=["Count","Mean","Sum"]
+    ) 
+    
+    if (color_stat == "Count"): 
+        y_poss_color_options_2 = ("#_Tweets")
+        color_option = "Retweet_Count"
+    else: 
+        y_poss_color_options_2 = ("Retweet_Count", "Quote_Count", "Like_Count", "Emoji_count", "Word_count")  
+        color_option =  st.selectbox(
+                "Select y var",
+                y_poss_color_options_2
+            )  
+    
+    var_time_plot_2 = plotting(df, histx_option_2, histy_option_2, stat_2, color_option, color_stat)
 
     def get_plot_time(var_time_plot): 
         var_time_plot.show_plot()
