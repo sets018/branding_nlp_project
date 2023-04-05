e34d78fe209f1e48b31cb0d4c31f7b6105d1b652
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-04 19:32:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,40 @@ import streamlit as st
 import plotly.figure_factory as ff
 from google.oauth2 import service_account
 from google.cloud import bigquery
+import plotly.express as px
 
+class user_input():
+    def __init__(self, var, type, data, type_data, input_list):
+        self.var = var
+        self.type = type
+        self.data = data
+        self.type_data = type_data
+        self.input_list = input_list
+        self.get_input()
+    def get_input(self):
+        self.user_input = 'placeholder'
+        if (self.type == 'radio'):
+            self.get_radio()
+        elif (self.type == 'slider'):
+            self.get_slider()
+        self.input_list.append(self.user_input)
+    def get_radio(self):
+        if (self.type_data == 'dataframe'):
+            self.user_input = st.radio(
+                self.var,
+                np.unique(self.data.data_source[self.var]))
+        elif (self.type_data == 'list'):
+            self.user_input = st.radio(
+                self.var,
+                self.data)
+        st.write(self.var,": ",self.user_input)
+    def get_slider(self):
+        if (self.type_data == 'dataframe'):
+            self.user_input = st.slider(self.var, 0, max(self.data.data_source[self.var]), 1)
+        elif (self.type_data == 'list'):
+            self.user_input = st.slider(self.var, 0, max(self.data), 1)
+        st.write(self.var,": ",self.user_input)
+        
 st.set_page_config(
     page_title="Twitter_EDA",
     layout="centered",
@@ -35,6 +68,16 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT * FROM `branding-nlp-project.twitter_data.all_tweets_merged`")
+rows = run_query("SELECT * FROM `branding-nlp-project.twitter_data.tweets_merged_all`")
 df = pd.DataFrame(rows)
+st.write("Data head")
 st.write(df.head())
+st.write("Data shape")
+st.write(df.shape())
+st.write("Data columns/vars")
+st.write(df.columns())
+st.write("Numerical variables stats")
+df_stats  = df.describe().apply(lambda s: s.apply(lambda x: format(x, 'f')))
+df_num_stats = df_stats[["Retweet Count",	"Quote Count",	"Like Count", "Word_count", "Emoji_count"]]
+st.dataframe(df_num_stats)
+st.write("Graphsssssssss")
